592275b4d08dfd4d6803d0f2825794386b404170
Gagandeep <ggagandeep@salesforce.com>
2022-04-26 10:17:45 +0530

    fix: callouts indented in list

--- vscode-preview/src/markdownCompiler.ts
+++ vscode-preview/src/markdownCompiler.ts
@@ -8,24 +8,22 @@ import { sfdocsCustomPlugin } from '@sfdocs-internal/sfdocs-directive-plugin';
 import { defListPlugin } from '@sfdocs-internal/definition-list-plugin';
 import * as remarkGfm from 'remark-gfm';
 import * as remarkFrontmatter from 'remark-frontmatter';
-import admonitions from 'remark-admonitions';
 import * as highlight from 'remark-highlight.js';
 import sfdocsRenderFunctions from './generic-directive-plugin/sfdocsDefaultRenderers';
 import genericRenderFunctions from './generic-directive-plugin/genericDefaultRenderers';
 
 export function markdownCompiler(currentFilePath: string) {
-
-	const sfdocsPlugin = sfdocsCustomPlugin(sfdocsRenderFunctions());
+	
+    const sfdocsPlugin = sfdocsCustomPlugin(sfdocsRenderFunctions());
     const genericPlugin = genericDirective(genericRenderFunctions());
-    const definitionListPlugin = defListPlugin();
+   const definitionListPlugin = defListPlugin();
     return remark()
             .use(remarkGfm)
             .use(remarkFrontmatter, { type: 'yaml', marker: '-' } as any)
-            .use(sfdocsPlugin)
             .use(internalReferencePlugin({}))
             .use(highlight)
             .use(remark_directive)
-            .use(admonitions)
             .use(genericPlugin)
+            .use(sfdocsPlugin)
             .use(definitionListPlugin);
 }
